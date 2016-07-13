d438bae17266c910bb23a1ff2f4eafd29eca1178
Luis Americo Auyadermont Muñoz <lauyader2011@gmail.com>
2016-07-12 21:41:41 -0400

    Servidor Web con NodeJS
    lee archivos html

--- servidor.js
+++ servidor.js
@@ -1,3 +1,14 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
